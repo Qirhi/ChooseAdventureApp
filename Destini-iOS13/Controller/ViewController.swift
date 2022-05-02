e03bcc7afcab14e2b7dac3b4dee003d2f04b929f
Rhiannon Stanford <rhiannonstanford@gmail.com>
2022-05-01 18:46:44 -0700

    Add values to variablestring values to variables and button labels.  Update on viewDidLoad

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -13,10 +13,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+    @IBOutlet weak var choiceMade: UIButton!
+    
+    let story0 = "You see a fork in the road."
+    let choice1 = "Take a left."
+    let choice2 = "Take a right."
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.storyLabel.text = self.story0
+        self.choice1Button.setTitle(self.choice1, for: .normal)
+        self.choice2Button.setTitle(self.choice2, for: .normal)
     }
 
 
